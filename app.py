3c4aac23eaac20030e97a2d9261b9a2f53046364
LINDA-SUN\linda <rachelsun5@gmail.com>
2025-01-07 06:34:46 +0800

    edit something about alarm

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from book import fetch_book
 from news import fetch_latest_news
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import vlc
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -27,7 +28,9 @@ class Log(db.Model):
     date = db.Column(db.Date, nullable=False)
     
 # 共享變數來控制鬧鐘音效的播放
+global alarm_thread , alarm_playing
 alarm_playing = False
+
 alarm_thread = None
 person_detected = False
 motion_detected_flag = False
@@ -35,23 +38,56 @@ vlc_process = None
 timer_thread = None  ##新增計時器的線程
 start_time = 0  ## 用來記錄計時開始的時間
 
+
 # vlc_path = "/usr/bin/vlc"  # VLC 安裝的路徑
-vlc_path = r"C:\Program Files\VideoLAN\VLC\vlc.exe"  # 使用正確的 VLC 路徑
+# vlc_path = r"C:\Program Files\VideoLAN\VLC\vlc.exe"  # 使用正確的 VLC 路徑
 
 def play_with_vlc():
-    """使用 VLC 播放 .m4a 音效檔案"""
-    global start_time, timer_thread  ##新增計時器
-    alarm_sound_path = os.path.join(BASE_DIR, 'static', 'music', 'alarm.m4a')  # 音效檔案路徑
+    global alarm_playing
+    # creating Instance class object
+    player = vlc.Instance('--input-repeat=999')
+    alarm_sound_path = os.path.join(BASE_DIR, 'static', 'music', 'alarm.m4a')
+    # creating a new media
+    media = player.media_new(alarm_sound_path)
 
-    try:
-        # 使用 subprocess 執行 VLC 播放
-        subprocess.Popen([vlc_path, alarm_sound_path, '--intf', 'dummy'])  # 使用 dummy 介面不顯示 VLC 視窗
-        print("音效正在播放...")
-        start_time = time.time()  ## 記錄開始時間
-        timer_thread = threading.Thread(target=update_timer) ##
-        timer_thread.start() ##
-    except Exception as e:
-        print(f"無法啟動 VLC 播放音效: {e}")
+    # creating a media player object
+    media_player = player.media_player_new()
+
+    media_player.set_media(media)
+
+    # start playing video
+    media_player.play()
+    while alarm_playing and player.is_playing() == 1:
+        sleep(0.5)
+
+    media_player.stop()
+    return
+    # """使用 VLC 播放 .m4a 音效檔案"""
+    # global start_time, timer_thread  ##新增計時器
+    # alarm_sound_path = os.path.join(BASE_DIR, 'static', 'music', 'alarm.m4a')  # 音效檔案路徑
+
+    # try:
+    #     # 使用 subprocess 執行 VLC 播放
+    #     subprocess.Popen([vlc_path, alarm_sound_path, '--intf', 'dummy'])  # 使用 dummy 介面不顯示 VLC 視窗
+    #     print("音效正在播放...")
+    #     start_time = time.time()  ## 記錄開始時間
+    #     timer_thread = threading.Thread(target=update_timer) ##
+    #     timer_thread.start() ##
+    # except Exception as e:
+    #     print(f"無法啟動 VLC 播放音效: {e}")
+
+
+# def stop_vlc_alarm():
+#     """停止 VLC 播放音效"""
+#     global vlc_process
+#     if vlc_process:
+#         vlc_process.terminate()
+#         vlc_process = None
+#         print("VLC 音效已停止")
+#     if timer_thread:
+#         timer_thread.join()  ## 等待計時器線程結束
+#         print("計時器已停止")
+
 
 def update_timer(): ##新增計時def
     """計時器更新時間"""
@@ -59,17 +95,6 @@ def update_timer(): ##新增計時def
         elapsed_time = time.time() - start_time
         print(f"播放時間：{elapsed_time:.2f} 秒")
         time.sleep(1)  # 每秒更新一次
-
-def stop_vlc_alarm():
-    """停止 VLC 播放音效"""
-    global vlc_process
-    if vlc_process:
-        vlc_process.terminate()
-        vlc_process = None
-        print("VLC 音效已停止")
-    if timer_thread:
-        timer_thread.join()  ## 等待計時器線程結束
-        print("計時器已停止")
         
 def save_time_to_logs():
     """將計時器的時間值儲存到資料庫中的 logs 表"""
@@ -88,7 +113,7 @@ def end_timer():##
     print("收到終止計時的請求")
 
     # 停止 VLC 播放音效並儲存時間
-    stop_vlc_alarm()
+    # stop_vlc_alarm()
 
     return jsonify({"status": "success", "message": "時間已儲存"})
 
@@ -110,31 +135,41 @@ def handle_motion_detected():
         return jsonify({"status": "success", "show_button": False})
 
 # 當手機發送 HTTP 請求時觸發
-@app.route('/alarm', methods=['POST'])
-def alarm():
-    global alarm_playing, motion_detected_flag
+@app.route('/alarmStart')
+def alarmStart():
+    global alarm_playing, motion_detected_flag,alarm_thread
     print("收到鬧鐘通知！")
-    try:
-        if not alarm_playing:
-            alarm_playing = True
-            #play_with_vlc()
+    if not alarm_thread:
+        alarm_playing = threading.Thread(target = play_with_vlc)
+        alarm_playing.start()
+        alarm_thread = True
+    return "play"
+    # try:
+    #     if not alarm_playing:
+    #         alarm_playing = True
+    #         #play_with_vlc()
 
-        # 如果偵測到人
-        if motion_detected_flag:
-            print("偵測到人！停止鬧鐘，顯示按鈕供跳轉。")
-            #stop_vlc_alarm()
-            alarm_playing = False
+    #     # 如果偵測到人
+    #     if motion_detected_flag:
+    #         print("偵測到人！停止鬧鐘，顯示按鈕供跳轉。")
+    #         #stop_vlc_alarm()
+    #         alarm_playing = False
         
-            # 返回顯示按鈕的指令
-            return jsonify({"status": "success", "show_button": True,"stop_alarm": True})
+    #         # 返回顯示按鈕的指令
+    #         return jsonify({"status": "success", "show_button": True,"stop_alarm": True})
     
-        # 偵測不到人則繼續警報
-        return jsonify({"status": "success", "show_button": False, "stop_alarm": False})
+    #     # 偵測不到人則繼續警報
+    #     return jsonify({"status": "success", "show_button": False, "stop_alarm": False})
     
-    except Exception as e:
-        print(f"警報處理過程發生錯誤: {e}")
-        return jsonify({"error": str(e)}), 500
-
+    # except Exception as e:
+    #     print(f"警報處理過程發生錯誤: {e}")
+    #     return jsonify({"error": str(e)}), 500
+@app.route("/stop")
+def stop():
+    global alarm_playing, motion_detected_flag,alarm_thread
+    if alarm_thread:
+        alarm_thread = False
+    return "stop"
     
 @app.route('/')
 def index():
@@ -144,6 +179,11 @@ def index():
 @app.route('/challenge')
 def challenge():
     return render_template('challenge.html')
+
+
+@app.route('/alarm')
+def alarm():
+    return render_template('alarm.html')
 
 @app.route('/rank')
 def rank():
