2ead8418f75e10372ac2f2a2bc7e1a238b96c01b
LINDA-SUN\linda <rachelsun5@gmail.com>
2025-01-07 11:40:09 +0800

    fix broadcask

--- app.py
+++ app.py
@@ -199,6 +199,11 @@ def rank():
 
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)  # 設置語音速度
+voices = engine.getProperty('voices')
+for voice in voices:
+    if 'zh' in voice.id:
+        engine.setProperty('voice', voice.id)
+        break
 
 def speak_text(text):
     engine.say(text)
